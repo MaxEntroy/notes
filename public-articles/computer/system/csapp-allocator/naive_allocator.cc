68d851b5403f7dd7ed1ca0fdbde1648618878f87
MaxEntroy <likang@tju.edu.cn>
2023-04-14 21:03:40 +0800

    notes: Update csapp-allocator.

--- public-articles/computer/system/csapp-allocator/naive_allocator.cc
+++ public-articles/computer/system/csapp-allocator/naive_allocator.cc
@@ -1,7 +1,29 @@
 #include "naive_allocator.h"
 
+#include <iostream>
+#include <new>
+
 namespace csapp {
 
+bool NaiveAllocator::Init() {
+  mem_heap = new(std::nothrow) char[kMaxHeap];
+  if (not mem_heap) {
+    std::cerr << "Allocation returned nullptr.\n";
+    return false;
+  }
+  mem_brk = mem_heap;
+  mem_max_addr = mem_brk + kMaxHeap;
+  return true;
+}
 
+void* NaiveAllocator::Sbrk(int incr) {
+  auto* old_sbrk = mem_brk;
+  if (incr < 0 or mem_brk + incr > mem_max_addr) {
+    std::cerr << "Sbrk failed. Ran out of memory.\n";
+    return nullptr;
+  }
+  mem_brk += incr;
+  return old_sbrk;
+}
 
 }  // namespace csapp
--- public-articles/computer/system/csapp-allocator/naive_allocator.h
+++ public-articles/computer/system/csapp-allocator/naive_allocator.h
@@ -1,25 +1,43 @@
 #pragma once
 
+#include <stdio.h>
+
 namespace csapp {
 
 class NaiveAllocator {
  public:
-  NaiveAllocator() {
+  NaiveAllocator() = default;
 
-  }
+  NaiveAllocator(const NaiveAllocator&) = delete;
+  NaiveAllocator& operator=(const NaiveAllocator&) = delete;
+
+  /**
+   * @brief Initialize the memory system model
+   * @return If successful, return true. Else return false.
+   */
+  bool Init();
+
+  /**
+   * @brief Simple model of the sbrk function.
+   * Extends the heap by incr bytes and returns the start address of the new area.
+   * In this model, the heap cannot be shrunk.
+   * @param incr
+   * @return If successful, return a pointer to allocated memory. Else return false.
+   */
+  void* Sbrk(int incr);
 
  private:
   /// Points to first byte of heap
-  char *mem_heap = nullptr;
+  char* mem_heap = nullptr;
 
   /// Points to last byte of heap plus 1
-  char *mem_brk = nullptr;
+  char* mem_brk = nullptr;
 
   /// Max legal heap addr plus 1
-  char *mem_max_addr = nullptr;
+  char* mem_max_addr = nullptr;
 
   /// The maximum block size
-  static constexpr int kMaxHeap = 1 << 32;
+  static constexpr size_t kMaxHeap = (1 << 20);
 };
 
 }  // namespace csapp
