aaec188f71516c8518156c447954bc87165336c6
MaxEntroy <likang@tju.edu.cn>
2023-04-18 11:51:07 +0800

    notes: compare-exchange is update.

--- public-articles/computer/programming/compare-exchange/README.md
+++ public-articles/computer/programming/compare-exchange/README.md
@@ -7,6 +7,7 @@ std::mutex io_mtx;
 void producer() {
   if (counter < kThres)
     ++counter;
+
   std::scoped_lock lock(io_mtx);
   std::cout << "thread:" << std::this_thread::get_id() << ", counter=" << counter << std::endl;
 }
@@ -59,7 +60,7 @@ bool compare_exchange_strong(T& expected,
                              std::memory_order failure ) noexcept;
 ```
 
-Atomically compares the object representation (until C++20)value representation (since C++20) of *this with that of expected, 
+>Atomically compares the object representation (until C++20)value representation (since C++20) of *this with that of expected, 
 and if those are bitwise-equal, replaces the former with desired (performs read-modify-write operation). Otherwise, loads the actual value stored in *this into expected (performs load operation).
 
 ```cpp
@@ -72,6 +73,44 @@ else {
     return false;
 }
 ```
+
+正确的解法如下：
+
+```cpp
+void producer() {
+  auto cur = counter.load(std::memory_order_relaxed);
+  if (cur < kThres)
+    counter.compare_exchange_strong(cur, cur + 1);
+
+  std::scoped_lock lock(io_mtx);
+  std::cout << "thread:" << std::this_thread::get_id() << ", counter=" << counter << std::endl;
+}
+/* final result:
+thread:0x16fed3000, counter=10
+thread:0x16fe47000, counter=10
+*/
+```
+
+此时没有race condition出现，无论程序执行多少遍，结果都是10
+
+其可能得执行顺序如下：
+
+1. Thread 1 reads counter value, which is 9， less than 10.
+2. Thread 2 reads counter value, which is also 9, less than 10.
+3. Thread 1 **atomically** compares cur with counter, they are equal(both are 9), and replace counter with cur + 1, finally return true.
+4. Thread 2 **atomically** compares cur with counter, they are not equal(cur is 9, but counter is 10), return false.
+
+从中我们可以看出来，thread2在尝试更新的时候，先判断counter是否发生了变化，如果没有，即证明thread1没有先执行，那么它可以在此基础上自增。实际是，发现counter发生了变化，所以不能在当前值的基础上自增(即用cur + 1更新)。如果要自增，需要更新cur值才行。
+
+所以，```compare_exchange```操作，有一个乍一看很奇怪的操作，即失败时，用atomic var的当前值，更新expected。我们可以理解为，如果判断失败了，用原子变量的最新值，更新了旧值。然后在最新的值的前提下再进行自增，此时就是没有问题的。
+
+
+>The weak forms (1-2) of the functions are allowed to fail spuriously, that is, act as if *this != expected even if they are equal. When a compare-and-exchange is in a loop, the weak version will yield better performance on some platforms.
+
+这也有个问题，既然相等，但是没有返回true。具体细节是什么？
+1.更新原子变量，返回false
+2.没更新原子变量，没更新expected，返回false
+2.没更新原子变量，更新expected，返回false
 
 ### Ref
 
--- public-articles/computer/programming/compare-exchange/main.cc
+++ public-articles/computer/programming/compare-exchange/main.cc
@@ -52,46 +52,76 @@ namespace cas1 {
 
 void producer() {
   auto cur = counter.load(std::memory_order_relaxed);
-  while (cur < kThres and not counter.compare_exchange_weak(cur, cur + 1));
+  if (cur < kThres)
+    counter.compare_exchange_strong(cur, cur + 1);
 
   std::scoped_lock lock(io_mtx);
   std::cout << "thread:" << std::this_thread::get_id() << ", counter=" << counter << std::endl;
 }
 
-void test() {
-  std::thread t1(producer);
-  std::thread t2(producer);
-
-  t1.join();
-  t2.join();
-}
-
-std::atomic<bool> is_running{true};
-void close() {}
-
-void worker1() {
-  auto cur_state = is_running.load(std::memory_order_relaxed);
-  if (cur_state == true) {
-    if (is_running.compare_exchange_strong(cur_state, false)) close();
-  }
-}
-
-void worker11() {
-  auto cur_state = is_running.load(std::memory_order_relaxed);
-  if (is_running.compare_exchange_strong(cur_state, false)) close();
-}
-
-void worker2() {
-  auto cur_state = is_running.load(std::memory_order_relaxed);
-  while (cur_state == true and not is_running.compare_exchange_weak(cur_state, false));
-  if (cur_state == true) close();
-}
-
 }  // namespace cas1
+
+
+//namespace cas1 {
+//
+//void producer() {
+//  auto cur = counter.load(std::memory_order_relaxed);
+//  while (cur < kThres and not counter.compare_exchange_weak(cur, cur + 1));
+//
+//  std::scoped_lock lock(io_mtx);
+//  std::cout << "thread:" << std::this_thread::get_id() << ", counter=" << counter << std::endl;
+//}
+//
+//void test() {
+//  std::thread t1(producer);
+//  std::thread t2(producer);
+//
+//  t1.join();
+//  t2.join();
+//}
+//
+//}  // namespace cas1
+//
+//namespace cas2 {
+//
+//std::atomic<bool> is_running{true};
+//void close_and_clean_up() { std::cout << "close_and_clean_up is done." << std::endl; }
+//
+//void worker() {
+//  auto cur_state = is_running.load(std::memory_order_relaxed);
+//  if (cur_state == true) {
+//    is_running.compare_exchange_strong(cur_state, false);
+//    close_and_clean_up();
+//  }
+//}
+//
+//void test() {
+//  std::thread t1(worker);
+//  std::thread t2(worker);
+//  std::thread t3(worker);
+//
+//  t1.join();
+//  t2.join();
+//  t3.join();
+//}
+//
+////void worker11() {
+////  auto cur_state = is_running.load(std::memory_order_relaxed);
+////  if (is_running.compare_exchange_strong(cur_state, false)) close();
+////}
+////
+////void worker2() {
+////  auto cur_state = is_running.load(std::memory_order_relaxed);
+////  while (cur_state == true and not is_running.compare_exchange_weak(cur_state, false));
+////  if (cur_state == true) close();
+////}
+//
+//}  // namespace cas2
 
 int main(void) {
   //non_cas::test();
-  //cas::test();
-  cas1::test();
+  cas::test();
+  //cas1::test();
+  //cas2::test();
   return 0;
 }
