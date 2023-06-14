f6e3381efea2e2cafe4831196212fff5dd1892ee
MaxEntroy <likang@tju.edu.cn>
2023-06-14 11:22:20 +0800

    notes: Update template-meta-programming.

--- public-articles/tech/programming/template-meta-programming/variatic_template.cc
+++ public-articles/tech/programming/template-meta-programming/variatic_template.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 //  non-variadic print function
 //  A declaration for the nonvariadic version of print must be in scope when
@@ -15,7 +17,21 @@ std::ostream& print(std::ostream& os, const T& t, const Args&... rest) {
   return print(os, rest...);
 }
 
+// print any type we don't otherwise handle
+template <typename T>
+std::string debug_rep(const T &t) {
+  std::ostringstream ret; // see § 8.3 (p. 321)
+  ret << "[Debug]" << t ; // uses T's output operator to print a representation of t
+  return ret.str(); // return a copy of the string to which ret is bound
+}
+
+template<typename... Args>
+std::ostream& err_msg(std::ostream& os, const Args... rest) {
+  return print(os, debug_rep(rest)...);
+}
+
 int main(void) {
   print(std::cout, 13, "hello", 0.618);
+  err_msg(std::cout, 13, "hello,world", 0.618);
   return 0;
 }
